e1dd41c63a888cd06fc1ad7177f040e343f0e10c
Serkan <snamli828@gmail.com>
2023-01-28 17:08:54 +0300

    new changes just added

--- lib/src/padding/paddinghorizontal.dart
+++ lib/src/padding/paddinghorizontal.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/cupertino.dart';
 
-class Padding10 extends StatelessWidget {
+class PaddingVertical extends StatelessWidget {
   final Widget child;
-  const Padding10({Key? key, required this.child}) : super(key: key);
+  const PaddingVertical({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal:10),
+      padding: const EdgeInsets.symmetric(horizontal: 10),
       child: child,
     );
   }
--- lib/utils_custom1.dart
+++ lib/utils_custom1.dart
@@ -1,25 +1,25 @@
-// ignore_for_file: unused_import
+// ignore_for_file: unused_export
 
 library utils;
 
-import './src/icon/custom_icon.dart';
-import './src/padding/padding10.dart';
-import './src/padding/paddinghorizontal.dart';
-import './src/padding/paddingvertical.dart';
-import './src/text/body_large.dart';
-import './src/util/assets_path.dart';
-import './src/sizedbox/sizedbox10.dart';
-import './src/sizedbox/sizedbox5.dart';
-import './src/sizedbox/sizedbox20.dart';
-import './src/sizedbox/sizedbox25.dart';
-import './src/sizedbox/sizedbox30.dart';
-import './src/sizedbox/sizedbox35.dart';
-import './src/sizedbox/sizedbox40.dart';
-import './src/text/body_medium.dart';
-import './src/text/body_small.dart';
-import './src/text/headline1.dart';
-import './src/text/headline2.dart';
-import './src/text/headline3.dart';
-import './src/text/headline4.dart';
-import './src/text/headline5.dart';
-import './src/text/headline6.dart';
+export './src/icon/custom_icon.dart';
+export './src/padding/padding10.dart';
+export './src/padding/paddinghorizontal.dart';
+export './src/padding/paddingvertical.dart';
+export './src/text/body_large.dart';
+export './src/util/assets_path.dart';
+export './src/sizedbox/sizedbox10.dart';
+export './src/sizedbox/sizedbox5.dart';
+export './src/sizedbox/sizedbox20.dart';
+export './src/sizedbox/sizedbox25.dart';
+export './src/sizedbox/sizedbox30.dart';
+export './src/sizedbox/sizedbox35.dart';
+export './src/sizedbox/sizedbox40.dart';
+export './src/text/body_medium.dart';
+export './src/text/body_small.dart';
+export './src/text/headline1.dart';
+export './src/text/headline2.dart';
+export './src/text/headline3.dart';
+export './src/text/headline4.dart';
+export './src/text/headline5.dart';
+export './src/text/headline6.dart';
